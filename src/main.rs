60099c5e5ba1c3ffd4864125cef4c8733ed4257b
paulpr0 <github@pr0.co.uk>
2020-04-19 19:44:47 +0100

    small cleanup

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,6 @@ struct Opts {
     chunk_size: usize,
 }
 
-//need to make the timeout take into acct elapsed time...
-
 async fn write_chunk<F: io::AsyncWrite + Unpin>(out: &mut F, buf:&[u8]) {
     out.write(format!("\r\n{}\r\n", buf.len()).as_bytes()).await.expect("Output error");
     out.write(buf).await.expect("Output error");
@@ -30,7 +28,6 @@ async fn do_stuff<I: io::AsyncRead + Unpin, O: io::AsyncWrite + Unpin>(
     loop {
         let res = if opts.max_wait>0 {
             match timeout_at(timeout_time, i.read(&mut buf[bytes_read..])).await {
-             //   match timeout(tokio::time::Duration::from_millis(opts.max_wait as u64), i.read(&mut buf[bytes_read..])).await {
                 Ok(Ok(n)) if n == 0 => break,
                 Ok(Ok(n)) => n,
                 Ok(Err(e)) => {
@@ -74,16 +71,13 @@ fn main() -> () {
 #[cfg(test)]
 mod test {
     use tokio_test::io::Builder;
-   // use tokio::io;
     use crate::{Opts, do_stuff};
     use std::time::Duration;
 
     #[test]
     fn split_read() {
-       // let error = io::Error::new(io::ErrorKind::Other, "test");
         let mut min = Builder::new()
             .read(b"hello ")
-          //  .read_error(error)
             .read(b"world!")
             .build();
         let mut mout = Builder::new()
@@ -93,14 +87,12 @@ mod test {
 
         tokio_test::block_on(
             do_stuff(opts, &mut min,&mut mout));
-
     }
 
     #[test]
     fn write_called_once_buf_full() {
         let mut min = Builder::new()
             .read(b"A test ")
-            //  .read_error(error)
             .read(b"world!")
             .build();
         let mut mout = Builder::new()
@@ -118,7 +110,6 @@ mod test {
     fn slow_output_does_not_drop_data() {
         let mut min = Builder::new()
             .read(b"A test ")
-            //  .read_error(error)
             .read(b"world!")
             .build();
         let mut mout = Builder::new()
@@ -132,7 +123,6 @@ mod test {
 
         tokio_test::block_on(
             do_stuff(opts, &mut min,&mut mout));
-
     }
 
 }
